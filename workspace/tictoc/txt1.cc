db3c7f23d1bfe545a3bf095970a7303ed68e9e8c
Veins (password is veins) <veins@instant-veins.car2x.org>
2023-06-12 14:16:46 +0000

    magnesium

--- workspace/tictoc/txt1.cc
+++ workspace/tictoc/txt1.cc
@@ -18,6 +18,7 @@ class Knot : public cSimpleModule
 {
   private:
    // bool role;
+    cMessage* sendNewMsg = nullptr;
 
   protected:
     // The following redefined virtual function holds the algorithm.
@@ -31,6 +32,9 @@ Define_Module(Knot);
 void Knot::initialize()
 {
   //  role = par("isMaster");
+  // If you are a master (isMaster==true), then you should schedule yourself to send messages every 80 ms
+    sendNewMsg = new cMessage();
+    // This can be done using scheduleAt(simTime(), sendNewMsg);
 
     if (par("isMaster")) {
 
@@ -38,23 +42,30 @@ void Knot::initialize()
 
         send(msg, "out", 0);
 
-    } else if(!par("isMaster")){
-
     }
-
 
 }
 
-void Knot::handleMessage(cMessage *msg)
+void Knot::handleMessage(cMessage *msg){
 
-{
+    // If the msg is sendNewMsg, new messages have to be sent to the slaves
+    // We need a priority for each message and send it directly
+    // That meens we use send(anotherMsg, "out", k)
+
+
+    // If msg is a response of a slave, then the latency is recorded
+
+    if(!par("isMaster")){
+
+        }
+
     int n = gateSize("out");
     int k = intuniform(0, n-1);
 
     msg->addPar("isMaster");
     msg->setTimestamp();
 
-    EV << "Forwarding message " << msg->getName() << msg->getArrivalTime() <<" on gate[" << k+1 << "]\n";
+    EV << "Forwarding message " << msg->getName() << msg->getTimestamp() <<" on gate[" << k+1 << "]\n";
 
     send(msg, "out",k); // send out the message–
 

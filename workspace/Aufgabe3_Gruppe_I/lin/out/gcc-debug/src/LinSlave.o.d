c850625f532ab31ef78b9916e2ca0e340be95bf4
febu1300 <buruktetemke@gmail.com>
2023-06-28 17:12:01 +0200

    hello

---
+++ workspace/Aufgabe3_Gruppe_I/lin/out/gcc-debug/src/LinSlave.o.d
@@ -0,0 +1,8 @@
+../out/gcc-debug/src/LinSlave.o: LinSlave.cc LinSlave.h LinNode.h \
+ /home/adm_bur7746m/omnetpp-5.7/include/omnetpp.h LinRequestFrame_m.h \
+ LinResponseFrame_m.h
+LinSlave.h:
+LinNode.h:
+/home/adm_bur7746m/omnetpp-5.7/include/omnetpp.h:
+LinRequestFrame_m.h:
+LinResponseFrame_m.h:
---
+++ workspace/Aufgabe3_Gruppe_I/lin/out/gcc-release/src/LinMaster.o.d
@@ -0,0 +1,8 @@
+../out/gcc-release/src/LinMaster.o: LinMaster.cc LinMaster.h LinNode.h \
+ /home/adm_bur7746m/omnetpp-5.7/include/omnetpp.h LinRequestFrame_m.h \
+ LinResponseFrame_m.h
+LinMaster.h:
+LinNode.h:
+/home/adm_bur7746m/omnetpp-5.7/include/omnetpp.h:
+LinRequestFrame_m.h:
+LinResponseFrame_m.h:

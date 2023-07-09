65f8278668946026a6cf306dff2a31bcc14e5cdf
febu1300 <buruktetemke@gmail.com>
2023-07-09 17:24:27 +0200

    sumo

---
+++ workspace/veins_mit_vorgabe5/Aufgabe4_Gruppe_I/scenarios/intersection/stripRoutes.pl
@@ -0,0 +1,19 @@
+#!/bin/perl
+
+my $file = "crossing.rou.xml";
+
+open FILE, $file;
+
+
+my $counter = 0;
+print "<routes>";
+while (<FILE>) {
+
+	if ($_ =~ /route (edges.*)$/) {
+		print "<route id=\"route$counter\" $1\n";
+		$counter++;
+	}
+}
+print "</routes>\n";
+
+close FILE;
